11ac40fc0bee6f6f1b05abf1ca8aa65a7c94e5b9
123 <pylliok1@mail.ru>
2018-05-25 14:55:03 +0300

    Добавил выполнение скачивания, заливку в таблицу, и удаления файла

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -28,33 +28,37 @@ namespace WpfApp1
             InitializeComponent();
         }
         public const string main = "https://vk.com";
-        private static readonly string AppName = "WpfApp1";
         private List<Parse> Liste = new List<Parse>();
+        public void Pars(OpenFileDialog dialog)
+        {
+            HSSFWorkbook hssfwb;
+            using (FileStream file = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read))
+            {
+                hssfwb = new HSSFWorkbook(file);
+            }
+            ISheet sheet = hssfwb.GetSheetAt(hssfwb.ActiveSheetIndex);
+            if (sheet != null)
+            {
+                for (int rowIndex = 0; rowIndex <= sheet.LastRowNum; rowIndex++)
+                {
+                    var row = sheet.GetRow(rowIndex);
+                    if (row != null && row.Cells.Count == 5 && row.Cells[4].CellType == CellType.String && !string.IsNullOrWhiteSpace(row.Cells[4].StringCellValue))
+                    {
+                        Liste.Add(new Parse { Name = row.Cells[1].StringCellValue, Price = row.Cells[4].StringCellValue.Replace("-", ",") });
+                    }
+                }
+            }
+        }// парсинг таблицы
         private void AddFile(object sender, RoutedEventArgs e)
         {
             Liste.Clear();
             OpenFileDialog dialog = new OpenFileDialog();
             if (dialog.ShowDialog() == true)
             {
-                HSSFWorkbook hssfwb;
-                using (FileStream file = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read))
-                {
-                    hssfwb = new HSSFWorkbook(file);
-                }
-                ISheet sheet = hssfwb.GetSheetAt(hssfwb.ActiveSheetIndex);
-                if (sheet != null)
-                {
-                    for (int rowIndex = 0; rowIndex <= sheet.LastRowNum; rowIndex++)
-                    {
-                        var row = sheet.GetRow(rowIndex);
-                        if (row != null && row.Cells.Count == 5 && row.Cells[4].CellType == CellType.String && !string.IsNullOrWhiteSpace(row.Cells[4].StringCellValue))
-                        {
-                            Liste.Add(new Parse { Name = row.Cells[1].StringCellValue, Price = row.Cells[4].StringCellValue.Replace("-", ",") });
-                        }
-                    }
-                }
+                Pars(dialog);
             }
-        }//выбор таблицы и парсинг её
+        }//выбор таблицы
+
         private static UserCredential Stream(String[] Scopes)
         {
             using (var stream =
@@ -75,82 +79,102 @@ namespace WpfApp1
             }
         }
 
-        private void AddMenu(object sender, RoutedEventArgs e)
+        public void Add()
         {
             string[] Scopes = { SheetsService.Scope.Spreadsheets };
             String spreadsheetId = "1IUrDULP4pzI8kioVAcl_5f1-B8mFdaahjuj8qwVe5dA";
-            UserCredential credential = Stream(Scopes);
+            string AppName = "WpfApp1";
+            using (var stream =
+                 new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                UserCredential credential;
+                string credPath = Environment.GetFolderPath(
+                    Environment.SpecialFolder.Personal);
+                credPath = System.IO.Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
 
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = AppName
-            });
-            List<Request> requests = new List<Request>();
-            List<Request> reques = new List<Request>();
-            BatchUpdateSpreadsheetRequest busrer = new BatchUpdateSpreadsheetRequest
-            {
-                Requests = reques
-            };
-            for (int i = 0; i < 900; i++)
-            {
-                List<CellData> Valurrr = new List<CellData>();
-                for (int j = 0; j < 2; j++)
-                {
-                    Valurrr.Add(new CellData { UserEnteredValue = new ExtendedValue { StringValue = "" } });
-                }
-                reques.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = i + 4, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Valurrr } }, Fields = "userEnteredValue" } });
-            }
-            service.Spreadsheets.BatchUpdate(busrer, spreadsheetId).Execute();
-            for (int i = 0; i < Liste.Count; i++)
-            {
-                List<CellData> Value = new List<CellData>();
-                for (int j = 0; j < 3; j++)
-                {
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
 
-                    if (j == 0) Value.Add(new CellData { UserEnteredValue = new ExtendedValue { StringValue = Liste[i].Name, } });
-                    if (j == 1) Value.Add(new CellData { UserEnteredValue = new ExtendedValue { StringValue = Liste[i].Price } });
-                    if (j == 2) Value.Add(new CellData { UserEnteredValue = new ExtendedValue { FormulaValue = "=SUM(D" + (i + 5) + ":" + (i + 5) + ")" } });
+                var service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = AppName
+                });
+                List<Request> requests = new List<Request>();
+                List<Request> reques = new List<Request>();
+                BatchUpdateSpreadsheetRequest busrer = new BatchUpdateSpreadsheetRequest
+                {
+                    Requests = reques
+                };
+                for (int i = 0; i < 900; i++)
+                {
+                    List<CellData> Valurrr = new List<CellData>();
+                    for (int j = 0; j < 2; j++)
+                    {
+                        Valurrr.Add(new CellData { UserEnteredValue = new ExtendedValue { StringValue = "" } });
+                    }
+                    reques.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = i + 4, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Valurrr } }, Fields = "userEnteredValue" } });
                 }
-                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = i + 4, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Value } }, Fields = "userEnteredValue" } });
-            }
-            List<CellData> Cash = new List<CellData>();
-            for (char c = 'D'; c <= 'Z'; c++)
-            {
-                Cash.Add(new CellData { UserEnteredValue = new ExtendedValue { FormulaValue = "=ROUNDUP(SUMPRODUCT($B5:$B;" + c + "5:" + c + ");1)" } });
-            }
-            requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 1, ColumnIndex = 3 }, Rows = new List<RowData> { new RowData { Values = Cash } }, Fields = "userEnteredValue" } });
-            List<CellData> Dates = new List<CellData>
+                service.Spreadsheets.BatchUpdate(busrer, spreadsheetId).Execute();
+                for (int i = 0; i < Liste.Count; i++)
+                {
+                    List<CellData> Value = new List<CellData>();
+                    for (int j = 0; j < 3; j++)
+                    {
+
+                        if (j == 0) Value.Add(new CellData { UserEnteredValue = new ExtendedValue { StringValue = Liste[i].Name, } });
+                        if (j == 1) Value.Add(new CellData { UserEnteredValue = new ExtendedValue { StringValue = Liste[i].Price } });
+                        if (j == 2) Value.Add(new CellData { UserEnteredValue = new ExtendedValue { FormulaValue = "=SUM(D" + (i + 5) + ":" + (i + 5) + ")" } });
+                    }
+                    requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = i + 4, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Value } }, Fields = "userEnteredValue" } });
+                }
+                List<CellData> Cash = new List<CellData>();
+                for (char c = 'D'; c <= 'Z'; c++)
+                {
+                    Cash.Add(new CellData { UserEnteredValue = new ExtendedValue { FormulaValue = "=ROUNDUP(SUMPRODUCT($B5:$B;" + c + "5:" + c + ");1)" } });
+                }
+                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 1, ColumnIndex = 3 }, Rows = new List<RowData> { new RowData { Values = Cash } }, Fields = "userEnteredValue" } });
+                List<CellData> Dates = new List<CellData>
             {
                 new CellData { UserEnteredValue = new ExtendedValue { StringValue = DateTime.Today.ToShortDateString() } }
             };
-            requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 0, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Dates } }, Fields = "userEnteredValue" } });
-            List<CellData> Sum = new List<CellData>
+                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 0, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Dates } }, Fields = "userEnteredValue" } });
+                List<CellData> Sum = new List<CellData>
             {
                 new CellData { UserEnteredValue = new ExtendedValue { StringValue = "Сумма" } },
             };
-            requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 1, ColumnIndex = 1 }, Rows = new List<RowData> { new RowData { Values = Sum } }, Fields = "userEnteredValue" } });
-            List<CellData> Info = new List<CellData>
+                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 1, ColumnIndex = 1 }, Rows = new List<RowData> { new RowData { Values = Sum } }, Fields = "userEnteredValue" } });
+                List<CellData> Info = new List<CellData>
             {
                 new CellData { UserEnteredValue = new ExtendedValue { StringValue = "Дополнительная информация" } },
             };
-            requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 2, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Info } }, Fields = "userEnteredValue" } });
-            List<CellData> Name = new List<CellData>
+                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 2, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Info } }, Fields = "userEnteredValue" } });
+                List<CellData> Name = new List<CellData>
             {
                 new CellData { UserEnteredValue = new ExtendedValue { StringValue = "Наименование" } },
                 new CellData { UserEnteredValue = new ExtendedValue { StringValue = "Цена" } },
             };
-            requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 3, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Name } }, Fields = "userEnteredValue" } });
-            List<CellData> SumFormula = new List<CellData>
+                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 3, ColumnIndex = 0 }, Rows = new List<RowData> { new RowData { Values = Name } }, Fields = "userEnteredValue" } });
+                List<CellData> SumFormula = new List<CellData>
             {
                 new CellData { UserEnteredValue = new ExtendedValue { FormulaValue = "=SUM(D2:2)" } }
             };
-            requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 1, ColumnIndex = 2 }, Rows = new List<RowData> { new RowData { Values = SumFormula } }, Fields = "userEnteredValue" } });
-            BatchUpdateSpreadsheetRequest busr = new BatchUpdateSpreadsheetRequest
-            {
-                Requests = requests
-            };
-            service.Spreadsheets.BatchUpdate(busr, spreadsheetId).Execute();
+                requests.Add(new Request { UpdateCells = new UpdateCellsRequest { Start = new GridCoordinate { SheetId = 0, RowIndex = 1, ColumnIndex = 2 }, Rows = new List<RowData> { new RowData { Values = SumFormula } }, Fields = "userEnteredValue" } });
+                BatchUpdateSpreadsheetRequest busr = new BatchUpdateSpreadsheetRequest
+                {
+                    Requests = requests
+                };
+                service.Spreadsheets.BatchUpdate(busr, spreadsheetId).Execute();
+            }
+        }
+
+        private void AddMenu(object sender, RoutedEventArgs e)
+        {
+            Add();
         } //загружает данные в гугл таблицу
 
         private void DownloadMenu(object sender, RoutedEventArgs e)
@@ -206,15 +230,20 @@ namespace WpfApp1
             {
                 browser.DownloadHandler = new DownloadHandler();
                 browser.ExecuteScriptAsync("saveDoc();");
+                browser.FrameLoadEnd -= Browser_FrameLoadEnd;
                 //Таблицу сохраняет где находится исполняемый файл
             }
         }
     }
     public class DownloadHandler : IDownloadHandler
     {
+
+        private List<Parse> Liste = new List<Parse>();
+
         public event EventHandler<DownloadItem> OnBeforeDownloadFired;
 
         public event EventHandler<DownloadItem> OnDownloadUpdatedFired;
+
 
         public void OnBeforeDownload(IBrowser browser, DownloadItem downloadItem, IBeforeDownloadCallback callback)
         {
@@ -225,6 +254,7 @@ namespace WpfApp1
                 using (callback)
                 {
                     callback.Continue(downloadItem.SuggestedFileName, showDialog: false);
+                    callback.Dispose();
                 }
             }
         }
@@ -232,6 +262,19 @@ namespace WpfApp1
         public void OnDownloadUpdated(IBrowser browser, DownloadItem downloadItem, IDownloadItemCallback callback)
         {
             OnDownloadUpdatedFired?.Invoke(this, downloadItem);
+            if (downloadItem.IsComplete)
+            {
+                Liste.Clear();
+                OpenFileDialog file = new OpenFileDialog
+                {
+                    FileName = downloadItem.FullPath
+                };
+                MainWindow main = new MainWindow();
+                main.Pars(file);
+                main.Add();
+                MessageBox.Show("Выполнено");
+                File.Delete(downloadItem.FullPath);
+            }
         }
     }
     public class Parse
